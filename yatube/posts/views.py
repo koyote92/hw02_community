ea5bc48ac56137d17a20ceee8129afb892244d57
Roman Inozemtsev <irs1992@yandex.ru>
2022-11-14 03:20:29 +0500

    Deleted unused imports

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.template import loader
 from .models import Post, Group
 
 # Create your views here.
